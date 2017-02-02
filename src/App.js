2bef0c50a845eb9d4c913de9efd5066cc472d6df
dominik791 <dominik.broj@gmail.com>
2017-02-02 20:35:42 +0100

    initial version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { objTreeSameProps } from './exemplaryObjects';
+import { findAll } from 'obj-traverse/lib/obj-traverse';
+let lib = require('obj-traverse/lib/obj-traverse')
+
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      obj: objTreeSameProps
+    };
+  }
+
+  componentDidMount() {
+    console.log(findAll);
+    console.log(lib);
+  }
+
+  generateObjToDisplay(obj) {
+    let indentSmall = {
+      marginLeft: 10
+    };
+    let indentBig = {
+      marginLeft: 20
+    };
+    return (
+      <span>
+        {'{'}
+          <div style={indentBig}>
+            {Object.keys(obj).map((prop, index) => prop === 'children' ?
+              <div key={index}>
+                children: [
+                  {obj[prop].map((child, index) =>
+                    <div key={index}>
+                      <div style={indentBig}>
+                        {this.generateObjToDisplay(child)}<span>{obj[prop].length-1!==index ? ',' : null }</span>
+                      </div>
+                    </div>
+                  )}
+                ]
+              </div> :
+              <div key={index}>{`${prop}: ${obj[prop]}`}<span>{Object.keys(obj).length-1!==index ? ',' : null}</span></div> )}
+          </div>
+        {'}'}
+      </span>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.generateObjToDisplay(this.state.obj)}
       </div>
     );
   }
