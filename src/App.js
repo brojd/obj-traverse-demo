7b01fbc039b3559d233a5ccfba5adfa358af59ef
dominik791 <dominik.broj@gmail.com>
2017-02-07 22:20:48 +0100

    add replacementObj input, style form

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ class App extends Component {
     super();
     this.handleFindObjKeyChange = this.handleFindObjKeyChange.bind(this);
     this.handleFindObjValueChange = this.handleFindObjValueChange.bind(this);
-    this.handleReplacementObjChange = this.handleReplacementObjChange.bind(this);
+    this.handleReplacementObjKeyChange = this.handleReplacementObjKeyChange.bind(this);
+    this.handleReplacementObjValueChange = this.handleReplacementObjValueChange.bind(this);
     this.handleButtonClick = this.handleButtonClick.bind(this);
     this.state = {
       obj: objTreeSameProps,
       objToFindByKey: '',
       objToFindByValue: '',
-      replacementObj: ''
+      replacementObjKey: '',
+      replacementObjValue: ''
     };
   }
 
@@ -28,16 +30,21 @@ class App extends Component {
     this.setState({ objToFindByValue: event.target.value });
   }
 
-  handleReplacementObjChange(event) {
-    this.setState({ replacementObj: event.target.value });
+  handleReplacementObjKeyChange(event) {
+    this.setState({ replacementObjKey: event.target.value });
+  }
+
+  handleReplacementObjValueChange(event) {
+    this.setState({ replacementObjValue: event.target.value });
   }
 
   handleButtonClick() {
     let objToFindBy = {};
+    let replacementObj = {};
     objToFindBy[this.state.objToFindByKey] = this.state.objToFindByValue;
-    //let replacementObj = JSON.parse(this.state.replacementObj);
+    replacementObj[this.state.replacementObjKey] = this.state.replacementObjValue;
     let newObj = Object.assign ({}, this.state.obj);
-    findAndModifyAll(newObj, 'children', objToFindBy, {aaa: 'aa'});
+    findAndModifyAll(newObj, 'children', objToFindBy, replacementObj);
     this.setState({ obj: newObj });
   }
 
@@ -73,12 +80,42 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <span>let obj = </span>{this.generateObjToDisplay(this.state.obj)}
-        objToFindBy key<input value={this.state.objToFindByKey} onChange={this.handleFindObjKeyChange} />
-        objToFindBy value<input value={this.state.objToFindByValue} onChange={this.handleFindObjValueChange} />
-        replacementObj<input value={this.state.replacementObj} onChange={this.handleReplacementObjChange} />
-        <button type='button' onClick={this.handleButtonClick}>Check!</button>
+      <div className="ui center aligned grid ">
+        <div className='ui fourteen wide left aligned column'>
+          <div className='ui grid'>
+            <section className='six wide column'>
+              <span>let obj = </span>{this.generateObjToDisplay(this.state.obj)}
+            </section>
+            <section className='six wide column'>
+              <div className='ui form'>
+                <div className='field'>
+                  <label>objToFindBy key</label>
+                  <input value={this.state.objToFindByKey} onChange={this.handleFindObjKeyChange} />
+                </div>
+                <div className='field'>
+                  <label>objToFindBy value</label>
+                  <input value={this.state.objToFindByValue} onChange={this.handleFindObjValueChange} />
+                </div>
+                <div className='field'>
+                  <label>replacementObj key</label>
+                  <input value={this.state.replacementObjKey} onChange={this.handleReplacementObjKeyChange} />
+                </div>
+                <div className='field'>
+                  <label>replacementObj value</label>
+                  <input value={this.state.replacementObjValue} onChange={this.handleReplacementObjValueChange} />
+                </div>
+                <div className='ui center aligned segment ot-funcPreview'>
+                  findAndModifyAll(obj, 'children',
+                  {`{ ${this.state.objToFindByKey}: ${this.state.objToFindByValue} }, `}
+                  {`{ ${this.state.replacementObjKey}: ${this.state.replacementObjValue} })`}
+                </div>
+                <div className='field'>
+                  <button type='button' onClick={this.handleButtonClick} className='ui blue button'>Check!</button>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
       </div>
 
     );
